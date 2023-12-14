0806bfd08ec5833be9c8cf9161ca5dc86bc2480d
seonwoo0808 <seonwoo0808@pentag.kr>
2023-12-14 21:52:07 +0900

    feat: added wakatime and nickname to aboutme page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,15 +1,25 @@
-import { UserCircleIcon, CalendarIcon } from '@heroicons/react/20/solid';
+import {
+  UserCircleIcon,
+  CalendarIcon,
+  SparklesIcon,
+} from '@heroicons/react/20/solid';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { AcademicCapIcon } from '@heroicons/react/24/outline';
 import ScreenShotImage from '../assets/images/github-screenshot.png';
 import AddtionalLink from '../constants/additionalLink';
+import axios from 'axios';
 
 const features = [
   {
     key: 'Name',
     value: '정선우',
     icon: UserCircleIcon,
+  },
+  {
+    key: 'Nickname',
+    value: '펜타곤',
+    icon: SparklesIcon,
   },
   {
     key: 'Birth',
@@ -22,12 +32,41 @@ const features = [
     icon: AcademicCapIcon,
   },
 ];
+const stats = [
+  { id: 1, name: 'Total Coding Time', value: '' },
+  { id: 2, name: 'Daily Average', value: '' },
+  { id: 3, name: 'Most Language of Month', value: '' },
+];
 
-export default function Example() {
+export default function AboutMe() {
+  axios
+    .get(
+      'https://wakatime.com/share/@PENTAGON/814f6085-90b4-4431-9901-1f49d0420024.json',
+    )
+    .then((Response) => {
+      document.getElementById('Total Coding Time')!.innerHTML =
+        Response.data.data.grand_total.human_readable_total_including_other_language;
+      document.getElementById('Daily Average')!.innerHTML =
+        Response.data.data.grand_total.human_readable_daily_average_including_other_language;
+    })
+    .catch((Error) => {
+      console.log(Error);
+    });
+  axios
+    .get(
+      'https://wakatime.com/share/@PENTAGON/0e58a7c2-ce6c-4121-8a04-404b4d33def8.json',
+    )
+    .then((Response) => {
+      document.getElementById('Most Language of Month')!.innerHTML =
+        Response.data.data[0].name;
+    })
+    .catch((Error) => {
+      console.log(Error);
+    });
   return (
     <div className="bg-white">
       <Header />
-      <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="overflow-hidden bg-white pt-24 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div className="lg:pr-8 lg:pt-4">
@@ -78,6 +117,33 @@ export default function Example() {
               height={2250}
             />
           </div>
+        </div>
+      </div>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto pb-20 lg:mx-0">
+            <h1 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Since <span className="text-indigo-700">DEC 08 2022</span>
+            </h1>
+          </div>
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+            {stats.map((stat) => (
+              <div
+                key={stat.id}
+                className="mx-auto flex max-w-xs flex-col gap-y-4"
+              >
+                <dt className="text-base leading-7 text-gray-600">
+                  {stat.name}
+                </dt>
+                <dd
+                  className="order-first text-2xl font-semibold tracking-tight text-gray-900 sm:text-4xl"
+                  id={stat.name}
+                >
+                  {stat.value}
+                </dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
       <Footer />
